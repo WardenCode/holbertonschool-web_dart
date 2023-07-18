eb9a51e539f98ebf7257f9c09be6f0d5347ff2d3
Diego Linares <diegojeanluck@hotmail.com>
2023-07-18 02:03:48 -0500

    7. Basketball Shots

---
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -0,0 +1,21 @@
+int calcTeamScore(Map<String, int> team) {
+  int normal_points = team['Free throws']!;
+  int double_points = team['2 pointer']! * 2;
+  int triple_points = team['3 pointer']! * 3;
+
+  return normal_points + double_points + triple_points;
+}
+
+int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
+  int teamAScore = 0;
+  int teamBScore = 0;
+
+  teamAScore = calcTeamScore(teamA);
+  teamBScore = calcTeamScore(teamB);
+
+  if (teamAScore == teamBScore) return 0;
+
+  if (teamAScore > teamBScore) return 1;
+
+  return 2;
+}
